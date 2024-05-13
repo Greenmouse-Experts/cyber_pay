b8f0a53d3ab935a28050549db64e18e8f8fb8b82
iammoses <inawo.moses@gmail.com>
2024-05-13 12:38:19 +0100

    link updated

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -4,7 +4,7 @@ import "../Stylesheet/downloadbtn.scss"
 
 const DownloadButton = ({ img, span, text, delay, style, color, radius, store}) => {
 
-  const link = text === "Google Play" ? 'https://play.google.com/store/search?q=cyberpay&c=apps' : 'https://apps.apple.com/ng/app/cyberpay/id6466426786'
+  const link = text === "Google Play" ? 'https://play.google.com/store/apps/details?id=com.cyberpay.cyberpay_mobile_2' : 'https://apps.apple.com/ng/app/cyberpay/id6466426786'
   return (
 
     <Link data-aos={style} data-aos-duration={delay}  to={link} className={`download_link flex w-fit ${radius ?  "rounded-2xl" : "rounded-lg"}  ${color === "black" ? "bg-[#000000]" : "bg-[#042E46]"}`} >
