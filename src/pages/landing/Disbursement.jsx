3c9c379185bc173f80cf3804f3b757e1058447b4
iammoses <inawo.moses@gmail.com>
2024-05-30 16:18:36 +0100

    disbursement changed

--- src/pages/landing/Disbursement.jsx
+++ src/pages/landing/Disbursement.jsx
@@ -1,4 +1,3 @@
-
 import "../../Stylesheet/pension.scss";
 import { useTheme } from "../../ThemeContext";
 import Heading from "../../layout/landing/Heading";
@@ -14,8 +13,79 @@ const Disbursement = () => {
         head="Disbursement Solution"
         body="When it comes to automating your payroll, we don’t believe in hidden charges or unnecessary delays. "
       />
-      <div className="padding flex lg:flex-row flex-col justify-between lg:gap-20 gap-10 items-center">
-        <div
+      <div className="padding grid lg:grid-cols-2 grid-cols-1 justify-between lg:gap-16 gap-10 items-center">
+        <div className="bg-white card-shadow dark:bg-bluePrimary dark:text-white rounded-md px-14 py-8 flex flex-col items-center  w-full">
+          <div className=" bg-bluePrimary p-4 rounded-full w-32 h-32 scale-90">
+            {" "}
+            <img src="/icons/dist-1.gif" alt="location" className=" scale-90" />
+          </div>
+          <div className=" flex flex-col gap-3 mt-3">
+            <h3 className="text-3xl font-semibold text-center mb-1">
+              Simplify payments for your business{" "}
+            </h3>
+            <p>
+              Our Disbursement Solution is your ultimate partner for all your
+              business payment requirements. Whether you’re settling payments
+              with suppliers, customers, or partners, we streamline the process
+              for you.{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-white card-shadow dark:bg-bluePrimary dark:text-white rounded-md px-14 py-8 flex flex-col items-center  w-full">
+          <div className=" bg-bluePrimary p-4 rounded-full w-32 h-32 scale-90">
+            {" "}
+            <img src="/icons/dist-2.gif" alt="location" className=" scale-90" />
+          </div>
+          <div className=" flex flex-col gap-3 mt-3">
+            <h3 className="text-3xl font-semibold text-center mb-1">
+              Efficiency at your fingertips
+            </h3>
+            <p>
+              Let’s settle your payments with the utmost convenience and
+              cost-effectiveness. Join the businesses for whom this is not just
+              a dream but a reality. We handle your payments seamlessly, so you
+              can focus on what truly matters.
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-white card-shadow dark:bg-bluePrimary dark:text-white rounded-md px-14 py-8 flex flex-col items-center  w-full">
+          <div className=" bg-bluePrimary p-4 rounded-full w-32 h-32 scale-90">
+            {" "}
+            <img src="/icons/dist-3.gif" alt="location" className=" scale-90" />
+          </div>
+          <div className=" flex flex-col gap-3 mt-3">
+            <h3 className="text-3xl font-semibold text-center mb-1">
+              Payroll, perfected
+            </h3>
+            <p>
+              When it comes to automating your payroll, we don’t believe in
+              hidden charges or unnecessary delays. We’re here to ensure a
+              hassle-free process, allowing you to empower your employees
+              without worry.
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-white card-shadow dark:bg-bluePrimary dark:text-white rounded-md px-14 py-8 flex flex-col items-center  w-full">
+          <div className=" bg-bluePrimary p-4 rounded-full w-32 h-32 scale-90">
+            {" "}
+            <img src="/icons/dist-4.gif" alt="location" className=" scale-90" />
+          </div>
+          <div className=" flex flex-col gap-3 mt-3">
+            <h3 className="text-3xl font-semibold text-center mb-1">
+              Customized Payment Workflows
+            </h3>
+            <p>
+              We understand that every situation is unique. That’s why we
+              empower you to craft tailored payment workflows. Seamlessly
+              integrate our API endpoints, and you’ll swiftly manage the
+              collection, transmission, and authentication of payments.
+            </p>
+          </div>
+        </div>
+        {/* <div
           className="lg:w-[40%] w-full "
           data-aos="fade-right"
           data-aos-duration="1000"
@@ -34,9 +104,9 @@ const Disbursement = () => {
             suppliers, customers, or partners, we streamline the process for
             you.
           </p>
-        </div>
+        </div> */}
       </div>
-      <div className="padding flex lg:flex-row flex-col-reverse justify-between lg:gap-20 gap-10 items-center">
+      {/* <div className="padding flex lg:flex-row flex-col-reverse justify-between lg:gap-20 gap-10 items-center">
         <div
           className="flex flex-col gap-8 lg:w-[60%] w-full"
           data-aos="fade-up"
@@ -101,7 +171,7 @@ const Disbursement = () => {
         >
           <img src="/img/disbursement-4.png" alt="" className="w-full" />
         </div>
-      </div>
+      </div> */}
       {/* <div className="padding flex lg:flex-row flex-col justify-between lg:gap-20 gap-10 items-center">
         <div
           className="lg:w-[40%] w-full "
@@ -125,15 +195,14 @@ const Disbursement = () => {
         </div>
       </div> */}
       <div className=" bg-[#FAFAF9] dark:bg-gray-900 dark:text-white padding text-center flex flex-col justify-center items-center mt-10 pb-20">
-      <h2 className="h2">Experience Payment Intelligence</h2>
+        <h2 className="h2">Experience Payment Intelligence</h2>
         <p className="para mt-5 xl:w-[70%] w-full text-center">
-        Discover the intelligent way to manage your payments with CyberPay.
-            It’s more than a solution; it’s a partnership that streamlines your
-            financial processes, so you can reach new heights. Discover the
-            future of payments with CyberPay today.
+          Discover the intelligent way to manage your payments with CyberPay.
+          It’s more than a solution; it’s a partnership that streamlines your
+          financial processes, so you can reach new heights. Discover the future
+          of payments with CyberPay today.
         </p>
       </div>
-     
     </div>
   );
 };
