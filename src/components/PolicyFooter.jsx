87df512e62ccb77629f5066824ba2da0a43eb331
iammoses <inawo.moses@gmail.com>
2024-05-31 16:00:52 +0100

    updated

--- src/components/PolicyFooter.jsx
+++ src/components/PolicyFooter.jsx
@@ -1,65 +1,53 @@
-
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 function PolicyFooter() {
   return (
-    <div className="padding flex lg:flex-row flex-col justify-center items-center gap-5">
-    <NavLink
-      to="/isms"
-      className=" bg-[#DD0A35] sm:w-[30rem] w-full px-5 py-3 flex flex-col gap-1 justify-between items-center rounded-2xl"
-      data-aos-easing="ease-in-sine"
-      data-aos="zoom-in"
-      data-aos-duration="1100"
-    >
-       <div className=" flex justify-end">
-        <img
-          src="/img/isms.png"
-          alt=""
-          className=" w-16 h-16 rounded-full"
-        />
-      </div>
-      <div className="para text-white flex items-center">
-        <span>ISMS - ISO</span>
-        <span> 27001 Information Security Policy</span>
-      </div>
-     
-    </NavLink>
-    <NavLink
-      to="/bcms"
-      className=" bg-bluePrimary sm:w-[30rem] w-full px-5 py-3 justify-between flex flex-col gap-1 items-center rounded-2xl"
-      data-aos-easing="ease-in-sine"
-      data-aos="zoom-in"
-      data-aos-duration="1100"
-    >
-      <div className=" flex items-center gap-4">
-        <img
-          src="/img/bcms.webp"
-          alt=""
-          className="  h-16 rounded-full "
-        />
-           <img
-            src="/img/audit-1.png "
-            alt=""
-            className="  bg-white   h-16 "
-          />
+    <div className="padding grid xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 justify-center items-center gap-5">
+      <NavLink
+        to="/isms"
+        className=" bg-[#DD0A35] w-full px-5 py-3 flex justify-between items-center rounded-2xl"
+        data-aos-easing="ease-in-sine"
+        data-aos="zoom-in"
+        data-aos-duration="1100"
+      >
+        <div className="para text-white flex flex-col w-[70%]">
+          <span>ISMS - ISO</span>
+          <span> 27001 Information Policy</span>
+        </div>
+        <div className="w-[30%] flex justify-end">
+          <img src="/img/isms.png" alt="" className=" w-20 h-20 rounded-full" />
+        </div>
+      </NavLink>
+      <NavLink
+        to="/bcms"
+        className=" bg-bluePrimary w-full px-5 py-3 justify-between flex items-center rounded-2xl"
+        data-aos-easing="ease-in-sine"
+        data-aos="zoom-in"
+        data-aos-duration="1100"
+      >
+        <div className="para text-white flex flex-col  w-[70%]">
+          <span>BCMS - ISO </span>
+          <span> 22301 Business Policy</span>
+        </div>
+        <div className="w-[30%] flex justify-end">
           <img
-            src="/img/audit-2.png"
+            src="/img/bcms.webp"
             alt=""
-            className="  bg-white  h-16 "
+            className=" w-20 h-20 rounded-full "
           />
+        </div>
+      </NavLink>
+      <div  className=" bg-bluePrimary w-full px-5 py-3 justify-center flex items-center rounded-2xl">
+        <img src="/img/audit-1.png " alt="" className="  bg-white rounded-md   h-20 " />
       </div>
-      <div className="para text-white flex items-center">
-        <span>BCMS - ISO </span>
-        <span> 22301 Business Continuity Policy</span>
+      <div  className=" bg-bluePrimary w-full px-5 py-3 justify-center flex items-center rounded-2xl">
+        <img src="/img/audit-2.png" alt="" className="  bg-white rounded-md  h-20 " />
       </div>
-    </NavLink>
-  </div>
-  )
+    </div>
+  );
 }
 
-export default PolicyFooter
-
-
+export default PolicyFooter;
 
 // <div className="md:w-[30%] w-full flex gap-5">
 // <NavLink
