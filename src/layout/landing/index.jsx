bd4b8313d399b0e819a507bd23a42cf36d0150bd
iammoses <inawo.moses@gmail.com>
2024-06-06 12:52:08 +0100

    updated

--- src/layout/landing/index.jsx
+++ src/layout/landing/index.jsx
@@ -11,7 +11,7 @@ const LandingLayout = () => {
       <ScrollToTop />
       <main className="">
         <Outlet />
-        <div className=" bottom-10 left-[-12.5rem] hover:left-2 transition-all  z-[9999999999] fixed  items-center flex flex-row-reverse gap-0 cursor-pointer ">
+        <div className=" bottom-10 left-[-11.5rem] hover:left-2 transition-all  z-[9999999999] fixed  items-center flex flex-row-reverse gap-0 cursor-pointer ">
           <img src="/icons/whatsapp.svg" alt="whatsapp" />
           <p className="bg-white py-3 px-7 rounded-xl h-fit dark:bg-gray-900">Send us Message</p>
         </div>
