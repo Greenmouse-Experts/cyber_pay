a3fd24681586be7dfa4b9c80feb98f1bffa5deab
iammoses <inawo.moses@gmail.com>
2024-08-22 14:10:53 +0100

    base url updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://cyberpay.victornwadinobi.com/api", 
+  baseURL: "https://cyberpay.greenmouseonline.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
